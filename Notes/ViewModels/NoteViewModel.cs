6789f1c22355da58d9be7a6d25c3b2294f82f5f7
Wikiniki15 <wiki.niki.olalla@gmail.com>
2024-12-16 19:08:44 -0500

    MVVM

---
+++ Notes/ViewModels/NoteViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+
+namespace Notes.ViewModels;
+
+internal class NoteViewModel : ObservableObject, IQueryAttributable
+{
+    private Models.Note _note;
+
+}
